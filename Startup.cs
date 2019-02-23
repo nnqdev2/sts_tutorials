9fe847230ab21bd0e52bf3fcb566966f4c740cc9
Nga QUAN <quan.nga@deq.state.or.us>
2019-02-23 08:33:05 -0800

    commented out stuff

--- Startup.cs
+++ Startup.cs
@@ -176,43 +176,43 @@ namespace sts_tutorials
             //);
 
 
-            app.Use(async (ctx, next) =>
-            {
-                var schemes = ctx.RequestServices.GetRequiredService<IAuthenticationSchemeProvider>();
-                var handlers = ctx.RequestServices.GetRequiredService<IAuthenticationHandlerProvider>();
-                foreach (var scheme in await schemes.GetRequestHandlerSchemesAsync())
-                {
-                    var handler = await handlers.GetHandlerAsync(ctx, scheme.Name) as IAuthenticationRequestHandler;
-                    if (handler != null && await handler.HandleRequestAsync())
-                    {
-                        // start same-site cookie special handling
-                        string location = null;
-                        if (ctx.Response.StatusCode == 302)
-                        {
-                            location = ctx.Response.Headers["location"];
-                        }
-                        else if (ctx.Request.Method == "GET" && !ctx.Request.Query["skip"].Any())
-                        {
-                            location = ctx.Request.Path + ctx.Request.QueryString + "&skip=1";
-                        }
+            //app.Use(async (ctx, next) =>
+            //{
+            //    var schemes = ctx.RequestServices.GetRequiredService<IAuthenticationSchemeProvider>();
+            //    var handlers = ctx.RequestServices.GetRequiredService<IAuthenticationHandlerProvider>();
+            //    foreach (var scheme in await schemes.GetRequestHandlerSchemesAsync())
+            //    {
+            //        var handler = await handlers.GetHandlerAsync(ctx, scheme.Name) as IAuthenticationRequestHandler;
+            //        if (handler != null && await handler.HandleRequestAsync())
+            //        {
+            //            // start same-site cookie special handling
+            //            string location = null;
+            //            if (ctx.Response.StatusCode == 302)
+            //            {
+            //                location = ctx.Response.Headers["location"];
+            //            }
+            //            else if (ctx.Request.Method == "GET" && !ctx.Request.Query["skip"].Any())
+            //            {
+            //                location = ctx.Request.Path + ctx.Request.QueryString + "&skip=1";
+            //            }
 
-                        if (location != null)
-                        {
-                            ctx.Response.StatusCode = 200;
-                            var html = $@"
-                        <html><head>
-                            <meta http-equiv='refresh' content='0;url={location}' />
-                        </head></html>";
-                            await ctx.Response.WriteAsync(html);
-                        }
-                        // end same-site cookie special handling
+            //            if (location != null)
+            //            {
+            //                ctx.Response.StatusCode = 200;
+            //                var html = $@"
+            //            <html><head>
+            //                <meta http-equiv='refresh' content='0;url={location}' />
+            //            </head></html>";
+            //                await ctx.Response.WriteAsync(html);
+            //            }
+            //            // end same-site cookie special handling
 
-                        return;
-                    }
-                }
+            //            return;
+            //        }
+            //    }
 
-                await next();
-            });
+            //    await next();
+            //});
 
             app.UseCors("default");
             app.UseIdentityServer();
